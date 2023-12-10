9375b38caa43970676b9ec530ae14e1bc3a4e6fc
ameya-6964 <belvalkar7@gmail.com>
2023-12-10 21:04:25 +0530

    Modifed index.js

--- week-2.2/express-todo-app/index.js
+++ week-2.2/express-todo-app/index.js
@@ -7,3 +7,16 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the 'public' directory
+app.use(express.static("public"));
+
+// Mounting todoRoutes
+app.get("/", (req, res) => {
+  res.send("<h1> Hello </h1>");
+});
+app.use("/todos", todoRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
