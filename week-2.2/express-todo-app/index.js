91d26a409e0e5b4e6225dadf8ab1f51beab00521
ameya-6964 <belvalkar7@gmail.com>
2023-12-10 22:10:32 +0530

    Changes

--- week-2.2/express-todo-app/index.js
+++ week-2.2/express-todo-app/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const todoRoutes = require("./routes/todoRoutes");
-
 const app = express();
 const port = 3000;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
